e15ff875f0eb53564cbb238a95988e123aaa880d
Isuru Eranga <155760914+ErangaOttachchige@users.noreply.github.com>
2025-09-08 19:40:14 +0530

    Create Emoji_converter.py

---
+++ Emoji_converter.py
@@ -0,0 +1,16 @@
+message = input(">")
+words = message.split(" ")
+# print(words)
+
+emojis = {
+  ":)": "😊",
+  ":(": "😔"
+}
+
+output = ""
+
+for word in words:
+  output += emojis.get(word, word) + " "
+  
+  
+print(output)

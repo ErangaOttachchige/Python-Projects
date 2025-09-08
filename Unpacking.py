6a3bdc7bdcf7479711658b892b8e132fad1a1005
Isuru Eranga <155760914+ErangaOttachchige@users.noreply.github.com>
2025-09-08 16:01:54 +0530

    Create Unpacking.py

---
+++ Unpacking.py
@@ -0,0 +1,18 @@
+# Unpacking
+
+
+coordinates = (1,2,3) #Tuple (immutable)
+
+# x = coordinates[0]
+# y = coordinates[1]
+# z = coordinates[2]
+
+
+x, y, z = coordinates  # this is exactly identical to what we have on lines 2-4
+# we are unpacking the tuple in to these variables
+
+print(x)
+print(y)
+print(z)
+
+# we can use this feature in list[] as well.

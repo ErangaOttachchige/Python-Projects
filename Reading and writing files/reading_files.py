064cb1411b810a1189ea6a3b96215aee27d4cbd9
Isuru Eranga <155760914+ErangaOttachchige@users.noreply.github.com>
2025-09-16 00:49:37 +0530

    Update reading_files.py
    practicing .read(), .readline(), .readlines(), .readable() methods

---
+++ Reading and writing files/reading_files.py
@@ -0,0 +1,19 @@
+
+
+employee_file = open(r"C:\Users\Eranga Ottachchige\Desktop\Python Basics Mosh\Python-Projects\Reading and writing files\employees.txt", "r")
+
+print(employee_file.readable())
+# print(employee_file.read())
+
+# print(employee_file.readline())
+# print(employee_file.readline())
+
+# print(employee_file.readlines())  # Puth them inisde in a list[]
+
+# print(employee_file.readlines()[2])
+
+
+for employee in employee_file.readlines():   # this creates a list of employees in the .txt file
+  print(employee)
+
+employee_file.close()

28c26edef796f85456cbefc2d2ea9a96b1414739
Isuru Eranga <155760914+ErangaOttachchige@users.noreply.github.com>
2025-09-17 17:05:09 +0530

    Object functions

--- Building a Multiple Choize Quiz/app.py
+++ Building a Multiple Choize Quiz/app.py
@@ -3,7 +3,7 @@ from Question import Question
 question_prompts = [
   "What color are apples?\n(a) Red/Green\n(b) Purple\n(c) Orange\n\n", 
   "What color are Bananas?\n(a) Teal\n(b) Magenta\n(c) Yellow\n\n", 
-  "What color are strawberries?\n(a) Yellow\n(b) Red\n(c) Blue\n\n"
+  "What color are strawberries?a\n(a) Yellow\n(b) Red\n(c) Blue\n\n"
 ]
 
 
---
+++ object functions/app.py
@@ -0,0 +1,7 @@
+from Student import Student
+
+student1 = Student("Jim", "Accounting", 3.1)
+student2 = Student("Pam", "Business", 3.8)
+
+print(student2.on_honor_roll())
+print(student1.on_honor_roll())
